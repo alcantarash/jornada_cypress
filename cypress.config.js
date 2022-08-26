ab3ddc53029dbcd979ee717dc37483e4db1200bf
Sheldon <sheldon.alcantara@ambevtech.com.br>
2022-08-26 16:48:10 -0300

    adjusts again

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,12 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
 
   projectId: "ekxbi9",
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config)
     },
     baseUrl: 'https://conexaoqa.herokuapp.com'
   },
